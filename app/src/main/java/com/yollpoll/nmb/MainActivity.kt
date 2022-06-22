2cb205a3bfc3c601a9525a00902dec6d4e2f6c44
yollpoll <403716879@qq.com>
2022-06-22 11:13:47 +0800

    新建项目

---
+++ app/src/main/java/com/yollpoll/nmb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yollpoll.nmb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
