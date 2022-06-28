c251314ea0835d2f45ddd0e919c4e3dc4243cb78
yollpoll <403716879@qq.com>
2022-06-28 14:30:33 +0800

    动态主题完成

--- app/src/main/java/com/yollpoll/nmb/MyApplication.kt
+++ app/src/main/java/com/yollpoll/nmb/MyApplication.kt
@@ -1,14 +1,40 @@
 package com.yollpoll.nmb
 
-import android.app.Application
+import android.annotation.SuppressLint
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.annotation.ChecksSdkIntAtLeast
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.DynamicColorsOptions
 import com.yollpoll.base.NMBApplication
-import com.yollpoll.framework.fast.FastApplication
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
+
 
 /**
  * Created by spq on 2022/6/22
  */
 @HiltAndroidApp
-class MyApplication: NMBApplication() {
+class MyApplication : NMBApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        // Apply dynamic color
+        if (isDynamicColorAvailable()) {
+            DynamicColors.applyToActivitiesIfAvailable(this)
+        } else {
+            DynamicColors.applyToActivitiesIfAvailable(
+                this,
+                DynamicColorsOptions.Builder().setThemeOverlay(R.style.NmbTheme_Overlay).build()
+            )
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    @ChecksSdkIntAtLeast(api = VERSION_CODES.S)
+    fun isDynamicColorAvailable(): Boolean {
+        if (VERSION.SDK_INT < VERSION_CODES.S) {
+            return false
+        }
+        return true
+    }
+
 }
